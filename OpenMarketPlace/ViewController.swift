0c4e82c755a446ceb7d83f23b027d544dbb60ddd
2OP <po>
2020-09-17 18:33:41 -0700

    Initial Commit

---
+++ OpenMarketPlace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenMarketPlace
+//
+//  Created by Shangrong Li on 9/17/20.
+//  Copyright © 2020 2OP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
